adc2fd92db01ccb16ab8cc50a5e7d40e4aeb2bf3
hugo-baptista <pg50416@alunos.uminho.pt>
2023-03-21 11:32:41 +0000

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Trabalho aqui</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
